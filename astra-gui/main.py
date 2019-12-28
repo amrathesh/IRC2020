f48f65807a181154a8b1b07014a35d11cbd0b72f
dragonfist453 <thermosphere453@gmail.com>
2019-12-28 11:40:28 +0530

    GUI first push

---
+++ astra-gui/main.py
@@ -0,0 +1,45 @@
+import flask
+from flask import request, jsonify
+import json
+import os
+import sys
+
+
+app = flask.Flask(__name__)
+battery_level = 100
+compass = -180
+gps = {"lat": None,"long":None}
+values = {'battery':battery_level,'compass':compass,'location':gps}
+@app.route("/")
+def admin_control():
+	return flask.render_template("admin_control.html")
+	
+@app.route("/ping")
+def ping():
+	response = os.system("ping -c 1 localhost");
+	if(response==0):
+		return "Connected"
+	else:
+		return "Not connected"	
+	
+@app.route("/set_values")
+def set_values():
+    global values
+    data_string = request.args.get('json')
+    data = json.loads(data_string)
+    values['battery'] = data['battery']
+    values['compass'] = data['compass']
+    values['location'] = data['location']
+    return values
+
+@app.route("/get_values")
+def get_values():
+    global values
+    battery_level = values['battery']
+    compass = values['compass']
+    gps = values['location']
+    return jsonify(values)
+	
+
+if __name__ == "__main__":
+    app.run(host = 'localhost', debug=True)
---
+++ astra-gui/server_test.py
@@ -0,0 +1,19 @@
+import sys,time
+import requests
+import json
+
+battery = 100
+compass = -180
+latitude = 13.0419608
+longitude = 77.5046096
+
+while(1):
+	time.sleep(0.5)
+	battery = (battery-1)%100
+	compass = (compass+10)%360
+	#longitude = longitude-0.000052
+	data = {"battery":battery,"compass":compass,"location":{"lat":latitude,"long":longitude}}
+	print(f"Battery: {battery}\nCompass: {compass}\n")
+	string_data = json.dumps(data)
+	r = requests.get("http://localhost:5000/set_values?json="+string_data)
+			
